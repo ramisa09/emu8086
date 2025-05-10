6a38660b86eccc1341d15ddf8ee7ad33a23e54e8
Syeda Ramisa Islam <syedaramisaislammim@gmail.com>
2025-05-10 07:54:36 +0600

    Update addition.asm

--- addition.asm
+++ addition.asm
@@ -1,52 +1,77 @@
 .model small
 .stack 100h
-.data 
-    a db 'Enter the first number : $'
-    b db 'Enter the second number : $'
-    sum db 'Summation = $'
-.code
-main proc  
-    mov ax,@data
-    mov ds,ax
-    
-    mov ah,9
-    lea dx,a
+.data
+    msg1 db 'Enter first digit: $'
+    msg2 db 'Enter second digit: $'
+    msg3 db 'Sum is: $'
+.code 
+main proc
+    mov ax, @data
+    mov ds, ax
+
+   
+    mov ah, 9
+    lea dx, msg1
     int 21h
-    
-    mov ah,1
+
+    mov ah, 1         
     int 21h
-    mov bh,al
-    
-    mov ah,2
-    mov dl,10
+    mov bl, al       
+
+   
+    mov ah, 2
+    mov dl, 13
     int 21h
-    mov dl,13
+    mov dl, 10
     int 21h
-    
-    mov ah,9
-    lea dx,b
-    int 21h
-    
-    mov ah,1
-    int 21h
-    add bh,al
-    sub bh,48
-    
-    mov ah,2
-    mov dl,10
-    int 21h
-    mov dl,13
-    int 21h
-    
-    mov ah,9
-    lea dx,sum
-    int 21h
+
      
-    mov ah,2
-    mov dl,bh
+    mov ah, 9
+    lea dx, msg2
     int 21h
+
+    mov ah, 1
+    int 21h
+    mov bh, al       
+
+   
+    add bl, bh      
+    mov al, bl
+    mov ah, 0
+    aaa               
+
+    mov bx, ax       
     
-    mov ah,4ch
+    mov ah, 2
+    mov dl, 13
     int 21h
-    main endp
+    mov dl, 10
+    int 21h
+
+  
+    mov ah, 9
+    lea dx, msg3
+    int 21h
+
+   
+    mov dl, bh
+    cmp dl, 0
+    je skip_tens
+    add dl, 48
+    mov ah, 2
+    int 21h 
+    
+    
+skip_tens:
+
+   
+    mov dl, bl
+    add dl, 48
+    mov ah, 2
+    int 21h
+
+exit:
+    mov ah, 4ch
+    int 21h
+main endp
 end main
